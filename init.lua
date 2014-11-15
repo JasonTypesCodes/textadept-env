8a16d62608818a45204df26c531d308235fca36d
Jason Schindler <jschind@gmail.com>
2014-11-15 12:59:06 -0600

    Changed font

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-ui.set_theme('base16-tomorrow-dark', {font ='Source Code Pro', fontsize = 11})
+ui.set_theme('base16-tomorrow-dark', {font ='Source Code Pro Semibold', fontsize = 11})
 
 -- Adds rust support
 textadept.file_types.extensions.rs = 'rust'
