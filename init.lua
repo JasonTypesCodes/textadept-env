55b7cb52936eda1bbf84bb16810b1aa4452930d8
unknown <schindjm@MMXL13907XL.us.maritz.net>
2014-12-03 16:30:48 -0600

    Only set the theme when we aren't in curses

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-ui.set_theme('base16-tomorrow-dark', {font ='Source Code Pro Semibold', fontsize = 11})
+if not CURSES then ui.set_theme('base16-tomorrow-dark', {font ='Source Code Pro Semibold', fontsize = 11}) end
 
 -- Adds rust support
 textadept.file_types.extensions.rs = 'rust'
