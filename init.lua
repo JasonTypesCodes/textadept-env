6c3c1cba2f3733de7c5904252300b47a5f4f1ac2
Jason Schindler <jschind@gmail.com>
2015-03-02 21:49:01 -0600

    Adds fish lexer

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
-if not CURSES then ui.set_theme('base16-tomorrow-dark', {font ='Source Code Pro Semibold', fontsize = 11}) end
+if not CURSES then ui.set_theme('base16-brewer-dark', {font ='Source Code Pro Semibold', fontsize = 11}) end
 
 -- Adds rust support
 textadept.file_types.extensions.rs = 'rust'
+textadept.file_types.shebangs.fish = 'fish'
