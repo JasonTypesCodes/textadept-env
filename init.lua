eec734a4c6c4fabd6e0ce4813dd904616dc8c3e5
Jason Schindler <jschind@gmail.com>
2015-03-12 10:51:34 -0500

    Updated for ta 7.9

--- init.lua
+++ init.lua
@@ -2,4 +2,3 @@ if not CURSES then ui.set_theme('base16-brewer-dark', {font ='Source Code Pro Se
 
 -- Adds rust support
 textadept.file_types.extensions.rs = 'rust'
-textadept.file_types.shebangs.fish = 'fish'
