7e2beb49ffc69a2d57047ce5b19db71acf2707da
Jason Schindler <jschind@gmail.com>
2015-04-29 12:26:41 -0500

    Adds swm module

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 if not CURSES then ui.set_theme('base16-brewer-dark', {font ='Source Code Pro Semibold', fontsize = 11}) end
 
+require('textadept_swm')
+
 -- Adds rust support
 textadept.file_types.extensions.rs = 'rust'
