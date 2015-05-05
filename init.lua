478762751323bbcd14ee5937d512eefec5dbe529
Jason Schindler <jschind@gmail.com>
2015-05-05 12:46:50 -0500

    Adds groovy lexer to gradle file extension

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
-if not CURSES then ui.set_theme('base16-brewer-dark', {font ='Source Code Pro Semibold', fontsize = 11}) end
+if not CURSES then 
+  ui.set_theme('base16-brewer-dark', {font ='Source Code Pro Semibold', fontsize = 11}) 
+end
 
 require('textadept_swm')
 
 -- Adds rust support
 textadept.file_types.extensions.rs = 'rust'
+
+-- Use Groovy lexer for gradle files
+textadept.file_types.extensions.gradle = 'groovy'
